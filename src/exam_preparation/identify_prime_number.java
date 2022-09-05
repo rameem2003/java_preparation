c7a327a96f39670173dfc0d11aa412c8760cea26
rameem2003 <rameem2019@gmail.com>
2022-09-05 14:46:37 +0600

    2 programme upload

---
+++ src/exam_preparation/identify_prime_number.java
@@ -0,0 +1,32 @@
+package exam_preparation;
+
+import java.util.Scanner;
+
+public class identify_prime_number {
+
+
+    // Identify the prime number
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+
+        System.out.print("Enter any positive number: ");
+        int num = input.nextInt();
+
+
+        for(int i = 2; i < num; i++){
+            if(num%i == 0){
+                count++;
+                break;
+            }
+        }
+
+
+        if(count == 0){
+            System.out.println(num + " is prime number");
+        }else{
+            System.out.println(num + " is not prime number");
+        }
+    }
+}
---
+++ src/exam_preparation/print_prime_number_in_range.java
@@ -0,0 +1,39 @@
+package exam_preparation;
+
+import java.util.Scanner;
+
+// Print prime number in range
+
+public class print_prime_number_in_range {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Enter the starting number: ");
+        int start = input.nextInt();
+
+        System.out.print("Enter the ending number: ");
+        int end = input.nextInt();
+
+        int count = 0;
+
+
+        for(int i = start; i <= end; i++){
+
+
+            for(int j = 2; j <= i - 1; j++){
+                if(i%j == 0){
+                    count++;
+                    break;
+                }
+            }
+
+
+            if(count == 0){
+                System.out.println(i);
+            }
+
+            count = 0;
+        }
+    }
+}
