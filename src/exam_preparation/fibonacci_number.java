b319e2d9569a97e0733ac9fae7e5c5c39256aae1
rameem2003 <rameem2019@gmail.com>
2022-09-06 08:59:51 +0600

    update

---
+++ src/exam_preparation/fibonacci_number.java
@@ -0,0 +1,31 @@
+package exam_preparation;
+
+import java.util.Scanner;
+
+
+// Fibonacci series
+
+
+public class fibonacci_number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter how many number: ");
+
+        int n = input.nextInt();
+
+        int first = 0;
+        int second = 1;
+
+        int fibo;
+
+        System.out.print(first + " " + second);
+
+        for(int i = 3; i<=n; i++){
+            fibo = first + second;
+            System.out.print(" " + fibo);
+
+            first = second;
+            second = fibo;
+        }
+    }
+}
